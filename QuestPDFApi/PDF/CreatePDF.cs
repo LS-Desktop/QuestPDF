5c55a3f4d173a99f0681d6a6f350d3173ebbafcc
CarlosLopezSoto <carlitoslopezs@hotmail.com>
2024-01-23 17:04:12 -0300

    feat!: create a pdf with api minimal

---
+++ QuestPDFApi/PDF/CreatePDF.cs
@@ -0,0 +1,64 @@
+﻿using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Fluent;
+using QuestPDF.Previewer;
+using Svg.Skia;
+using QuestPDFApi.PDF.ExtensionToSVG;
+
+namespace QuestPDFApi.PDF
+{
+    public class CreatePDF
+    {
+        public static void GeneratePdf()
+        {
+            //Logo ls svg 
+            using var svg = new SKSvg();
+            svg.Load("Images/LSlogo.svg");
+
+            //License
+            QuestPDF.Settings.License = LicenseType.Community;
+            // code in your main method
+            var document = Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20));
+
+                    page.Header()
+                        .Text("Crea el PDF!")
+                        .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+                    page.Content()
+                .PaddingVertical(1, Unit.Centimetre)
+                .Column(x =>
+                {
+                    x.Spacing(20);
+
+                    x.Item().Text("Esto es una prueba de que funciona");
+                    x.Item().Text("Desde la api");
+
+                    x.Item().Svg(svg);
+                });
+                    
+                    page.Footer()
+                        .AlignCenter()
+                        .Text(x =>
+                        {
+                            x.DefaultTextStyle(x => x.FontSize(10));
+                            x.Span("Page ");
+                            x.CurrentPageNumber();
+                        });
+                });
+            });
+            // instead of the standard way of generating a PDF file
+            document.GeneratePdf("hello.pdf");
+
+            // use the following invocation
+            document.ShowInPreviewer();
+
+        }
+    }
+}
---
+++ QuestPDFApi/PDF/ExtensionToSVG/SvgExtensions.cs
@@ -0,0 +1,23 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+using Svg.Skia;
+
+namespace QuestPDFApi.PDF.ExtensionToSVG
+{
+    public static class SvgExtensions
+    {
+        public static void Svg(this IContainer container, SKSvg svg)
+        {
+            container
+                .AlignCenter()
+                .AlignMiddle()
+                .ScaleToFit()
+                .Width(svg.Picture.CullRect.Width)
+                .Height(svg.Picture.CullRect.Height)
+                .Canvas((canvas, space) => canvas.DrawPicture(svg.Picture));
+        }
+    }
+}
+
+
+
